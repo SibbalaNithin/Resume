ad80274ace6bcb7c5c8de4595e9b6719490b4247
Nithin Sibbala <sibbalanithin227@gmail.com>
2024-12-22 16:01:22 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Project1/Header'
+import Footer from './Project1/Footer'
+import Home from './Project1/Home'
+import Service from './Project1/Service'
+import Skills from './Project1/Skills'
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Footer></Footer>
+      <Home></Home>
+      <Service></Service>
+      <Skills></Skills>
+    </div>
+  )
+}
+
+export default App
