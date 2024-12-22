1b2b66f46baca308bcd1220a270f4c96283b11b0
Nithin Sibbala <sibbalanithin227@gmail.com>
2024-12-22 16:02:08 +0530

    Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Main.css'
+const Home = () => {
+  return (
+    <div className='Home'>
+        <div className='ProfilePic'><img src="https://th.bing.com/th/id/OIP.3gBvmi-7wOgLMugWQE2GqQHaHa?w=168&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="profile pic" /></div>
+        <div className='ProfileInfo'>
+            <h1>Nithin S</h1>
+            <p>Software Developer</p>
+            <p>BTech</p>
+            <p>Piler, Andhra Pradesh</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
